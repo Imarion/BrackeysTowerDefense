575f668962e57562caf243c322dcdbc1ffbcf4b7
emr <renquin@awtce.be>
2019-01-09 13:44:48 +0100

    Episodes 5, 6 and 7

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour {
 
+	private Transform target;
+
+	[Header("Attributes")]
 	public float range = 15f;
+	public float fireRate = 1f;
+	private float fireCountdown = 0f;
+
+	[Header("Unity setup fields")]
 	public string enemyTag = "Enemy";
+
 	public Transform partToRotate;
 	public float turnSpeed = 10f;
 
-	private Transform target;
+	public GameObject bulletPrefab;
+	public Transform firePoint;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +35,14 @@ public class Turret : MonoBehaviour {
 		Quaternion lookRotation = Quaternion.LookRotation(dir);
 		Vector3 rotation = Quaternion.Lerp (partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
 		partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+
+		if (fireCountdown <= 0f) {
+			Shoot ();
+			fireCountdown = 1f / fireRate;
+		}
+
+		fireCountdown -= Time.deltaTime;
 
 	}
 
@@ -46,6 +63,15 @@ public class Turret : MonoBehaviour {
 			target = nearestEnemy.transform;
 		} else {
 			target = null;
+		}
+	}
+
+	void Shoot() {
+		GameObject bulletGO = (GameObject)Instantiate (bulletPrefab, firePoint.position, firePoint.rotation);
+		Bullet bullet = bulletGO.GetComponent<Bullet> ();
+
+		if (bullet != null) {
+			bullet.Seek (target);
 		}
 	}
 
