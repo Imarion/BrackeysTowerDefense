f409695017bd2c9518d68847f8aea93f6c47c0df
emr <renquin@awtce.be>
2019-01-11 13:23:42 +0100

    Missile + explosion (radius) + effect

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityScript.Steps;
 
 public class Bullet : MonoBehaviour {
 
 	public float speed = 70f;
+	public float explosionRadius = 0f;
 	public GameObject impactEffect;
 
 	private Transform target;
@@ -25,6 +27,7 @@ public class Bullet : MonoBehaviour {
 		}
 
 		transform.Translate (dir.normalized * distanceThisFrame, Space.World);
+		transform.LookAt (target);
 	}
 
 	public void Seek(Transform _target) {
@@ -33,9 +36,34 @@ public class Bullet : MonoBehaviour {
 
 	void HitTarget() {
 		GameObject effectIns = (GameObject)Instantiate (impactEffect, transform.position, transform.rotation);
-		Destroy (effectIns, 2f);
+		Destroy (effectIns, 5f);
 
-		Destroy (target.gameObject);
+		if (explosionRadius > 0f) {
+			Explode ();
+			
+		} else {
+			Damage (target);
+		}
+
 		Destroy (gameObject);
+	}
+
+	void Explode() {
+		Collider[] colliders = Physics.OverlapSphere (transform.position, explosionRadius);
+
+		foreach (Collider collider in colliders) {
+			if (collider.tag == "Enemy") {
+				Damage (collider.transform);
+			}
+		}
+	}
+
+	void Damage(Transform enemy) {
+		Destroy (enemy.gameObject);
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere (transform.position, explosionRadius);
 	}
 }
