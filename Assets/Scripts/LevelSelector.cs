d90c6b41b7f6176137fbf956b0cb49b2b3a0010e
emr <renquin@awtce.be>
2019-01-21 11:22:39 +0100

    Episode 26: level select

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LevelSelector : MonoBehaviour
+{
+
+	public SceneFader fader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void Select(String levelName) {
+		fader.FadeTo (levelName);
+	}
+}
